cb3056926652c4fa3cbfa0e88b6155342693b2ce
ssimhan <sandhya.simhan@gmail.com>
2026-01-18 11:24:27 -0800

    Setup + radhe shyam

---
+++ data.js
@@ -0,0 +1,80 @@
+const RECITAL_DATA = [
+    {
+        id: 'brindavanam',
+        name: 'Brindavanam',
+        subtitle: 'Older Kids',
+        songs: [
+            {
+                id: 'radhe-shyam',
+                title: 'Radhe Shyam',
+                lyrics: `Radhe shyama radhe Shyam
+Radha Madhava radhe Shyam (x2)
+
+Adi Narayana radhe Shyam (x2)
+Anadi purusha Megha Shyam (x2)
+
+Vasudeva Hari radhe Shyam (x2)
+Vasuki Shayana Megha Shyam (x2) 
+
+Vasudeva Hari radhe Shyam
+Vasuki Shayana Megha Shyam 
+
+Radhe shyama radhe Shyam
+Radha Madhava radhe Shyam (x2)
+
+Shrisha Ramesh radhe Shyam (x2)
+Keshava Achutha Megha Shyam (x2)
+
+Sesha Shayana Hari radhe Shyam (x2)
+
+Dasaki shavanutha Megha Shyam 
+Dasaki shavanutha radhe Shyam
+
+Radhe shyama radhe Shyam
+Radha Madhava radhe Shyam (x3)`,
+                videos: [
+                    { title: 'Learning Module', url: 'https://share.descript.com/embed/s83BqlbfQze' }
+                ]
+            },
+
+            {
+                id: 'narayana-suktam',
+                title: 'Narayana Suktam',
+                lyrics: `ṛtagṃ sa̠tya-mpa̍ra-mbra̠hma̠ pu̠ruṣa̍m-kṛṣṇa̠piṅga̍lam ।
+ū̠rdhvarē̍taṃ vi̍rūpā̠kṣa̠ṃ vi̠śvarū̍pāya̠ vai namō̠ nama̍ḥ ॥
+nnā̠rā̠ya̠ṇāya̍ vi̠dmahē̍ vāsudē̠vāya̍ dhīmahi ।
+tannō̍ viṣṇuḥ prachō̠dayā̎t ॥`,
+                videos: [
+                    { title: 'Learning Module 1', url: '' }
+                ]
+            }
+        ]
+    },
+    {
+        id: 'balagokulam',
+        name: 'Balagokulam',
+        subtitle: 'Year 1 Kids',
+        songs: [
+            {
+                id: 'govinda-krishna-jai',
+                title: 'Govinda Krishna Jai',
+                lyrics: `(Lyrics coming soon...)
+
+Govinda Krishna Jai, Gopala Krishna Jai
+Govinda Govinda Govinda Krishna Jai
+...`,
+                videos: [
+                    { title: 'Learning Module 1', url: '' }
+                ]
+            },
+            {
+                id: 'maadumekam',
+                title: 'Maadumekam',
+                lyrics: `(Lyrics coming soon...)`,
+                videos: [
+                    { title: 'Learning Module 1', url: '' }
+                ]
+            }
+        ]
+    }
+];
